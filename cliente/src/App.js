87403d9da8cd8b5e44a02a51e4c3d22d60ffe299
brayan <brodriguez20071@gmail.com>
2025-08-19 12:14:37 -0500

    Initial commit

---
+++ cliente/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="container text-center mt-5">
+      <h1 className="text-primary">Hola Bootstrap en React 🎉</h1>
+      <button className="btn btn-success segundary">Click aquí</button>
+    </div>
+    
+
+  );
+}
+
+
+
+export default App;
