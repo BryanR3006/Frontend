907b6b68e8c9112ac7c64156e0b09d790477acaf
brayan <brodriguez20071@gmail.com>
2025-08-26 11:37:06 -0500

    (Feat): Servidor mysql server.js

--- server/server.js
+++ server/server.js
@@ -1,26 +1,22 @@
 import express from "express";
-import { connectDB } from "./config/db.js"; 
+import { connectDB } from "./config/db.js"; // ajusta la ruta si tu archivo está en otra carpeta
 
 const app = express();
 const PORT = process.env.PORT || 4000;
 
 const startServer = async () => {
   try {
-    
+    // Conectar a MySQL
     await connectDB();
 
     // Rutas de prueba
     app.get("/", (req, res) => {
-      res.send("Servidor FuncionNDO");
+      res.send("Servidor y MySQL funcionando 🚀");
     });
-
-    app.get("/Prodcutos", (req,res)=>{
-        r/)
-    })
 
     // Iniciar servidor
     app.listen(PORT, () => {
-      console.log(` Servidor corriendo en http://localhost:${PORT}`);
+      console.log(`✅ Servidor corriendo en http://localhost:${PORT}`);
     });
   } catch (error) {
     console.error("❌ No se pudo iniciar el servidor:", error);
