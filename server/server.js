7e9e238e61ef21327dc28602a275e2f1628aacbc
brayan <brodriguez20071@gmail.com>
2025-08-26 11:30:17 -0500

    (Feat): Servidor funcional y frontend

--- server/server.js
+++ server/server.js
@@ -1,43 +1,31 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
-const bodyParser = require('body-parser');
-const sql = require('mssql');
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
+import express from "express";
+import { connectDB } from "./config/db.js"; 
+
 const app = express();
+const PORT = process.env.PORT || 4000;
 
-app.use(cors());
-app.use(bodyParser.json());
+const startServer = async () => {
+  try {
+    
+    await connectDB();
 
-// Configuración del servidor de BD
-const dbConfig = {
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    server: process.env.DB_SERVER,
-    database: process.env.DB_DATABASE,
-    options: {
-        encrypt: true,
-        trustServerCertificate: true
-    }
+    // Rutas de prueba
+    app.get("/", (req, res) => {
+      res.send("Servidor FuncionNDO");
+    });
+
+    app.get("/Prodcutos", (req,res)=>{
+        r/)
+    })
+
+    // Iniciar servidor
+    app.listen(PORT, () => {
+      console.log(` Servidor corriendo en http://localhost:${PORT}`);
+    });
+  } catch (error) {
+    console.error("❌ No se pudo iniciar el servidor:", error);
+  }
 };
 
-// Conexión con la base de datos
-sql.connect(dbConfig, (err) => {
-    if (err) {
-        console.log("❌ Error en la conexión:", err);
-    } else {
-        console.log("✅ Conexión exitosa con callback");
-    }
-});
+startServer();
 
-// Rutas
-app.get('/', (req, res) => {
-    res.send('API de autenticación y autorización');
-});
-
-// Puerto
-const Port = process.env.PORT || 5000;
-app.listen(Port, () => {
-    console.log(`Corriendo por el puerto ${Port}`);
-});
